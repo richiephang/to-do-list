f942f2e442c7d45e140bce3d1ea4d3bb7c02225a
Phang Chi Yang <richiephang01@gmail.com>
2022-05-08 03:31:15 +0800

    New updates for Heroku deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#xq472!4%zh(u&m!93qtbpqlkjssy5^o$uc(8r8ya7in-ijp+g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "https://glacial-thicket-51750.herokuapp.com/"]
 
 
 # Application definition
