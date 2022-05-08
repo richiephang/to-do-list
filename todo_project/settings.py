fb71667c807ecf22d17e5fbb4461ab3af8553bd2
Phang Chi Yang <richiephang01@gmail.com>
2022-05-08 16:51:00 +0800

    java script background

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -122,4 +122,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from todolist.views import todoView, addTodo, deleteTodo
 # from todolist.views import TaskList
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     # path('todolist/', todoView),
     # path('addTodo/' , addTodo),
     # path('deleteTodo/<int:todo_id>/' , deleteTodo),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
