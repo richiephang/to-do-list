74e7bf5b010f9807447dbc8473ef249df43c45fd
Phang Chi Yang <richiephang01@gmail.com>
2022-05-08 17:11:45 +0800

    heroku static bug

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
